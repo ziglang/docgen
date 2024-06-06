15c1421118924f6dc1924bc453b45f3db4c7bbd2
Andrew Kelley <andrew@ziglang.org>
2024-06-06 12:45:50 -0700

    add docgen tool; rename to docgen

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# doctest
+# docgen
 
 A tool for generating documents such as
 [Zig's language reference](https://ziglang.org/documentation/0.12.0/) or
 [release notes](https://ziglang.org/download/0.12.0/release-notes.html).
 
 This repository is provided as a Zig package for convenience. The canonical
-source lives
-[in the zig compiler repository](https://github.com/ziglang/zig/blob/master/tools/doctest.zig).
+source lives in the zig compiler repository:
+* [doctest.zig](https://github.com/ziglang/zig/blob/master/tools/doctest.zig).
+* [docgen.zig](https://github.com/ziglang/zig/blob/master/tools/docgen.zig).
--- build.zig
+++ build.zig
@@ -4,23 +4,39 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
+    const doctest_exe = b.addExecutable(.{
         .name = "doctest",
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/doctest.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(exe);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+    const docgen_exe = b.addExecutable(.{
+        .name = "docgen",
+        .root_source_file = b.path("src/docgen.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    b.installArtifact(doctest_exe);
+    b.installArtifact(docgen_exe);
+
+    const doctest_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/doctest.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const docgen_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/docgen.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_doctest_unit_tests = b.addRunArtifact(doctest_unit_tests);
+    const run_docgen_unit_tests = b.addRunArtifact(docgen_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_doctest_unit_tests.step);
+    test_step.dependOn(&run_docgen_unit_tests.step);
 }
